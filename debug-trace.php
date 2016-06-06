75622d18b81d99d351ff10165e5df80ef085b34f
Stephen J. Carnam <steve@steveorevo.com>
2016-06-06 13:16:11 -0700

    Original Debug and Trace

---
+++ debug-trace.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Debug and Trace
+Plugin URI: http://serverpress.com/plugins/debug-trace
+Description: Forces WP_DEBUG = true and enables cross-platform/language trace statement in PHP and JavaScript. Use the Trace window for debugging.
+Author: Stephen Carnam
+Version: 2.0.0
+Author URI: http://steveorevo.com/
+*/
